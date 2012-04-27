ccb95fcda0c68b58418716a357255b8789cc6f92
Adam G <ghanem.adam@gmail.com>
2012-04-28 00:51:57 +0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '4e8aae6b078cb89ab20224a391383d259bc03189266e537359f417712e6ca5e898680ba1d39ac6afd3d6e01fe27c3d90d3664d671040a56199b80e2bb9099d7a'
